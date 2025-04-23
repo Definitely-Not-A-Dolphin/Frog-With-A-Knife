7d212f35c4beabb0dfde41eb19856a13af63eddf
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2025-04-23 23:09:41 +0200

    Initiated sendMeme.cjs
    The first attempts for meme posting by the bot have commenced.

---
+++ commands/utility/sendMeme.cjs
@@ -0,0 +1,12 @@
+/* import placeholder from './../../memes.json' assert {type: 'json'};
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('memePlaceholder')
+    .setDescription('Gives you an image!'),
+  async execute(interaction) {
+    await interaction.reply(placeholder.link);
+  },
+}; */
