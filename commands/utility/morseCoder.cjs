ac41f3b984458b992a9da045bd0877c1a3879b4d
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2025-05-02 13:33:57 +0200

    Started MorseCoder
    A small side thingey

---
+++ commands/utility/morseCoder.cjs
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const table = [
+  ["a", "⸱-"],
+  ["b", "⸱---"],
+  ["c", "-⸱-⸱"],
+  ["d", "-⸱⸱"],
+  ["e", "⸱"],
+  ["f", "⸱⸱-⸱"],
+  ["f", "--⸱"],
+  ["g", "--⸱"],
+  ["h", "⸱⸱⸱⸱"],
+  ["i", "⸱⸱"],
+  ["j", "⸱---"],
+  ["k", "-⸱-"],
+  ["l", "⸱-⸱⸱"],
+  ["m", "--"],
+  ["n", "-⸱"],
+  ["o", "---"],
+  ["p", "⸱--⸱"],
+  ["q", "--⸱-"],
+  ["r", "⸱-⸱"],
+  ["s", "⸱⸱⸱"],
+  ["t", "-"],
+  ["u", "⸱⸱-"],
+  ["v", "⸱⸱⸱-"],
+  ["w", "⸱--"],
+  ["x", "-⸱⸱-"],
+  ["y", "-⸱--"],
+  ["z", "--⸱⸱"],
+];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption(option =>
+      option.setName('input')
+        .setDescription('The input to echo back')
+        .setRequired(true)),
+  async execute(interaction) {
+    await interaction.reply(/* Wat moet hier??? */);
+  },
+};
