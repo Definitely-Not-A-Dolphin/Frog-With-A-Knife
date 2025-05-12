b1b23f8f059064147bdbce8d965f959909ff54f4
Derek Verduijn <derek.verduijn@outlook.com>
2025-05-12 18:12:58 +0200

    Last commit of the branch: getMeme is finally working.

--- commands/utility/getMeme.js
+++ commands/utility/getMeme.js
@@ -1,30 +1,49 @@
-import memeData from "./../commandFiles/getMeme/memes.json" with { type: "json"};
-import { AttachmentBuilder, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import memeData from "./../commandFiles/getMeme/memes.json" with { type: "json" };
+import {
+  AttachmentBuilder,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("get-meme")
-  .setDescription("Replies with Pong!");
-
-const file = new AttachmentBuilder("./commands/commandFiles/getMeme/memes/" + memeData.array[0].attachment)
-  .setName(memeData.array[0].name)
-  .setDescription(memeData.array[0].description);
-
-const exampleEmbed = new EmbedBuilder()
-  .setTitle("Some title")
-  .setImage("attachment://" + memeData.array[0].attachment);
+  .setDescription("Gives you a meme!")
+  .addIntegerOption((option) =>
+    option.setName("input").setDescription("enter a number").setRequired(true),
+  );
 
 export async function execute(interaction) {
+  const input = interaction.options.getInteger("input") - 1;
 
+  if (!(1 <= input <= memeData.array.length)) {
+    await interaction
+      .reply({
+        content: "Ja dat werkt niet zo",
+      })
+      .then((/* response */) =>
+        console.log(
+          `Gave ${interaction.user.username} an non-extistant meme (input ${input}).`,
+        ),)
+      .catch(console.error);
+  }
+
+  const file = new AttachmentBuilder(
+    "./commands/commandFiles/getMeme/DCBotmemes/" +
+      memeData.array[input].attachment,
+  )
+    .setName(memeData.array[input].name)
+    .setDescription(memeData.array[input].description);
+
+  const exampleEmbed = new EmbedBuilder()
+    // .setTitle("Some title")
+    .setImage("attachment://" + memeData.array[input].attachment);
   await interaction
     .reply({
       withResponse: true,
       embeds: [exampleEmbed],
-      files: [file.attachment]
+      files: [file.attachment],
     })
     .then((/* response */) =>
-      console.log(
-        `Gave ${interaction.user.username} meme # .`,
-      )
-    )
+      console.log(`Gave ${interaction.user.username} meme number ${input}.`),)
     .catch(console.error);
-} 
+}
--- commands/utility/morseCoder.js
+++ commands/utility/morseCoder.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
-import converse from "../commandFiles/morseTable.json" with { type: "json"};
+import converse from "../commandFiles/morseTable.json" with { type: "json" };
 
 function morseEncoder(message) {
   message = message.toLowerCase();
@@ -10,35 +10,42 @@ function morseEncoder(message) {
     newMessage += converse.toMorse[message[i]];
     if (message[i + 1] !== " ") {
       newMessage += " ";
-    };
-  };
+    }
+  }
 
   newMessage = newMessage.substring(0, newMessage.length - 1);
 
   if (newMessage.includes("undefined", "")) {
-    newMessage += "\n" + "-# This translation contains characters I haven't figured out yet.";
+    newMessage +=
+      "\n" +
+      "-# This translation contains characters I haven't figured out yet.";
     newMessage = newMessage.replace(/undefined/g, "#");
-  };
+  }
 
   return newMessage;
-};
+}
 
 export const data = new SlashCommandBuilder()
   .setName("morsecoder")
   .setDescription("Encodes a message into Morse code!")
-  .addStringOption(option =>
-    option.setName("input")
+  .addStringOption((option) =>
+    option
+      .setName("input")
       .setDescription("The input to encode")
-      .setRequired(true)
+      .setRequired(true),
   );
 
 export async function execute(interaction) {
   const input = interaction.options.getString("input");
-  await interaction.reply({
-    content: morseEncoder(input), withResponse: true
-  })
-    .then((response) => console.log(
-      `Encoded ${input} to ${response.resource.message.content} for ${interaction.user.username}.`
-    ))
+  await interaction
+    .reply({
+      content: morseEncoder(input),
+      withResponse: true,
+    })
+    .then((response) =>
+      console.log(
+        `Encoded ${input} to ${response.resource.message.content} for ${interaction.user.username}.`,
+      ),
+    )
     .catch(console.error);
 }
